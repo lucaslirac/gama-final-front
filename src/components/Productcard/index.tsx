3aeacd87996e218f44d415efdf4166185aa12825
Lucas <lucas_lirac@outlook.com>
2023-03-07 09:03:08 -0300

    editando card

--- src/components/Productcard/index.tsx
+++ src/components/Productcard/index.tsx
@@ -13,10 +13,9 @@ function ProductCard() {
             
         </Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+          <ProductList />
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button variant="primary">Comprar</Button>
       </Card.Body>
     </Card>
   );
