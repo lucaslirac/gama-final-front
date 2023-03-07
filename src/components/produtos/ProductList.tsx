bbf992f81ded7dd3c864be31962f0618946ec658
Lucas <lucas_lirac@outlook.com>
2023-03-07 08:21:07 -0300

    Utilizando Api

--- src/components/produtos/ProductList.tsx
+++ src/components/produtos/ProductList.tsx
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ProductItem from './ProductItem';
+import { getproductList } from '../../service/api';
 
-interface Product {
-  id: number;
-  name: string;
-  price: number;
-}
+// interface Product {
+//   id: number;
+//   name: string;
+//   price: number;
+// }
 
-interface ProductListProps {
-  products: Product[];
-}
+// interface ProductListProps {
+//   products: Product[];
+// }
 
-const ProductList: React.FC<ProductListProps> = ({ products }) => {
+const ProductList: React.FC = () => {
+
+  const [productList, setProductList] = useState<any[]>([])
+
+useEffect(() => {
+  const fetchProductList = async () => {
+    const allProducts = await getproductList()
+    setProductList(allProducts.products)
+  }
+  fetchProductList()
+}, [])
+
+console.log(productList)
+
   return (
     <div>
-      {products.map(product => (
-        <ProductItem key={product.id} name={product.name} price={product.price} />
+      {productList.map(product => (
+        <>
+        <h1>{product.name}</h1>
+        </>
       ))}
     </div>
   );
