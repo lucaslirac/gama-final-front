aa1b8938170669ebcb30e26951782e186066943d
Lucas <lucas_lirac@outlook.com>
2023-03-08 16:31:38 -0300

    Add Cart page

---
+++ src/pages/cart/carrinho.style.ts
@@ -0,0 +1,26 @@
+import  styled, {css}  from "styled-components";
+
+export const Container = styled.div`
+margin-top:20px;
+padding-left:50px;
+padding-right:50px;
+.parte2{
+    padding-top: 50px;
+}
+.imageBook{
+    max-width: 90px;
+}
+.botao-excluir{
+    font-size:15px;
+    }
+.total{
+    margin-top:30px;
+    font-weight:bold;
+}
+.resumo-pedido{
+padding: 50px;
+}
+p{
+    font-size: 12px;
+}
+        `;
---
+++ src/pages/cart/index.tsx
@@ -0,0 +1,111 @@
+import Header from '../../components/header';
+import Footer from '../../components/footer';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Form from 'react-bootstrap/Form';
+import Stack from 'react-bootstrap/Stack';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Button from 'react-bootstrap/Button';
+import * as C from './carrinho.style';
+
+import ProductList from '../../components/produtos/ProductList';
+
+export default function Carrinho() {
+  return (
+    <div>
+      <header>
+        <Header />
+      </header>
+      <main>
+        <C.Container>
+          <Row>
+
+            <Col sm={7}>
+              <h2>Meu Carrinho</h2>
+              <h6>Confira os itens selecionados em seu carrinho de compras:</h6>
+
+              <>
+                {['sm', 'sm', 'sm'].map((breakpoint) => (
+                  <ListGroup key={breakpoint} horizontal={breakpoint} className="my-2 mt-3">
+                   
+                      <ListGroup.Item>
+                        <img src='' alt="foto capa livro" className='imageBook' />
+                      </ListGroup.Item>
+                   
+
+                    <ListGroup.Item>
+                      <div className="nomeLivro">
+                       
+                      </div>
+                    </ListGroup.Item>
+
+                    <ListGroup.Item>
+                      
+                    </ListGroup.Item>
+
+
+                    <ListGroup.Item><Button variant="light">Excluir</Button>{' '}</ListGroup.Item>
+                  
+                  </ListGroup>
+                ))}
+              </>
+            </Col>
+
+
+            <Col sm={5} className="resumo-pedido">
+              <h5>Resumo do Pedido</h5>
+
+              <InputGroup className="mb-4" >
+                <Form.Control
+                  placeholder="Insira o código de desconto aqui"
+                  aria-label="Código de Desconto"
+                  aria-describedby="basic-addon2"
+                />
+              </InputGroup>
+
+
+              <Row>
+                <Col xs={12} md={8}>
+                  Subtotal
+                </Col>
+                <Col xs={6} md={4}>
+                  R$ xxx
+                </Col>
+              </Row>
+
+              <Row>
+                <Col xs={12} md={8}>
+                  Envio
+                </Col>
+               
+              </Row>
+
+              <div className="total">
+                <Row>
+                  <Col xs={12} md={8}>
+                    Total
+                  </Col>
+                  <Col xs={6} md={4}>
+                    R$ xxx
+                  </Col>
+                </Row>
+              </div>
+              <br />
+              <Col xs={6} md={4}>
+                  <button>Terminar compra</button>
+                </Col>
+            </Col>
+          </Row>
+        </C.Container>
+
+      </main>
+
+      <footer>
+       <Footer />
+      </footer>
+    </div>
+
+  );
+};
