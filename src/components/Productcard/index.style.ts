bf89b9a55bca442f9df2be3811307e08bc2ae548
Lucas <lucas_lirac@outlook.com>
2023-03-09 10:20:42 -0300

    new card

---
+++ src/components/Productcard/index.style.ts
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+.card{
+        
+       
+        margin: 10px;
+        margin-bottom: 40px;
+        border: 1px solid #ccc;
+        & img{
+            width: 90%;
+            margin: auto;
+            border-radius: 8px;
+        }
+        & h5 {
+            text-align: center;
+            color: #00796B;
+            font-weight: 600;
+        }
+    }
+    ul li{
+        
+        margin: auto;
+        text-align: center;
+        border: none;
+    }
+    .container{
+        
+        display: grid;
+        grid-template-columns: 25% 25% 25% 25%;
+        background-color: #f1f1f1;
+        border: none;
+        margin: 100px auto; 
+    }
+    .links{
+        text-align: center;
+        & a {
+            text-decoration: none;
+            font-size: 13px;
+            color: #000;
+            padding: 3px;
+            border: 1px solid #ccc;
+            border-radius: 6px;
+        }
+ 
+    }
+`;
