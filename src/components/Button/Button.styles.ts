c43e573efa84147e2faa3dae7a8e1a715df22047
Lucas <lucas_lirac@outlook.com>
2023-03-10 12:22:59 -0300

    button

---
+++ src/components/Button/Button.styles.ts
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+interface ButtonProps {
+    display?: string;
+    size?: string;
+    weight?: string;
+    textTransform?: string;
+    width?: string;
+    height?: string;
+    padding?: string;
+    marginTop?: string;
+    borderRadius?: string;
+    border?: string;
+    backgroundColor?: string;
+    hoverBg?: string;
+}
+
+export const ButtonDefault = styled.button<ButtonProps>`
+    display: ${(props) => props.display};
+    font-size: ${(props) => props.size ? props.size : "1rem"};
+    font-weight: ${(props) => props.weight ? props.weight : 600};
+    color: var(--first-color);
+    text-transform: ${(props) => props.textTransform};
+    width: ${(props) => props.width};
+    height: ${(props) => props.height};
+    border-radius: .75rem;
+    padding: ${(props) => props.padding};
+    margin-top: ${(props) => props.marginTop};
+    border-radius: ${(props) => props.borderRadius};
+    border: ${(props) => props.border ? props.border : "none"};
+    background-color: ${(props) => props.backgroundColor};
+    transition: .5s;
+    cursor: pointer;
+    &:hover {
+        background-color:  ${(props) => props.hoverBg};
+    }
+    a {
+        display: block;
+        width: 100%;
+        height: 100%;
+        padding: 1rem 0;
+    }
+`
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,44 @@
+import { ButtonDefault } from "./Button.styles";
+
+interface ButtonProps {
+    display?: string;
+    size?: string;
+    weight?: string;
+    textTransform?: string;
+    width?: string;
+    height?: string;
+    padding?: string;
+    marginTop?: string;
+    borderRadius?: string;
+    border?: string;
+    backgroundColor?: string;
+    hoverBg?: string;
+    text?: string | JSX.Element;
+    type?: "submit" | "button" | "reset";
+    disabled?: boolean;
+    action?: () => void;
+  
+}
+
+export function Button(props: ButtonProps) {
+    return (
+        <ButtonDefault
+            display={props.display}
+            size={props.size}
+            weight={props.weight}
+            textTransform={props.textTransform}
+            width={props.width}
+            height={props.height}
+            padding={props.padding}
+            marginTop={props.marginTop}
+            borderRadius={props.borderRadius}
+            border={props.border}
+            backgroundColor={props.backgroundColor}
+            hoverBg={props.hoverBg}
+            type={props.type ? props.type : "button"}
+            onClick={props.action}
+        >
+            {props.text}
+        </ButtonDefault>
+    );
+}
