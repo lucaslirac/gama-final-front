b284f846682a47da857b96a046f7d2bfad8af9a7
Lucas <lucas_lirac@outlook.com>
2023-03-10 02:04:09 -0300

    adionando texto e btn

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -4,18 +4,27 @@ import Footer from "../../components/footer";
 import Carrosel from "../../components/carrosel";
 import ProductList from "../../components/produtos/ProductList";
 import { ProductsCarousel } from "../../components/ProductsCarousel";
+import { useNavigate } from "react-router-dom";
+import "./style.css";
 
 
 
 
 
 function Home() {
+    const navigate = useNavigate();
     return (
         <div className="App">
 
             <header>
                 <Header />
             </header>
+
+            <div className="cta">
+                <h1>Melhores livros pra voce</h1>
+                <p>Compre os melhores livros dos melhores autores do mundo</p>
+                <button onClick={() => navigate("/Products")}>Comprar</button>
+            </div>
 
             <div>
                 <div>
