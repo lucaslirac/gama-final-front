11f0b47ee737d4362c08c994836260cd3d7ac5ff
Lucas <lucas_lirac@outlook.com>
2023-03-10 12:21:16 -0300

    criacao de pagina de checkout

---
+++ src/pages/checkout/index.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Header from "../../components/header";
+import Footer from "../../components/footer";
+import { useNavigate } from "react-router-dom";
+import { MultiStepForm } from "../../components/MultistepForm/MultistepForm";
+
+
+function Check() {
+    const navigate = useNavigate();
+    return (
+        <div className="App">
+
+            <header>
+                <Header />
+            </header>
+
+
+            <div>
+                <div>
+                    <MultiStepForm />
+                </div>
+            </div>
+            <footer>
+                <Footer />
+            </footer>
+
+        </div>
+
+    )
+}
+
+export default Check
