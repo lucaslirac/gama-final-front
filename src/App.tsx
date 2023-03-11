df4ddcb9b3f5c6dc3bb240b247c52153608fe353
Lucas <lucas_lirac@outlook.com>
2023-03-11 13:18:23 -0300

    AuthProvider

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,24 @@ import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Routes from './routes/routes';
 import GlobalStyle from './styles/global';
-
+import { AuthProvider } from './context/AuthProvider/AuthProvider';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  /**
+   * UseEffect
+   * 
+   * Verificar no localstorage se existe usuario?
+   * Se tem usuario, tu usa o setAuth do context.
+   */
 
   return (
     <div className="App">
+      <AuthProvider>
         <Routes />
         <GlobalStyle />
+      </AuthProvider>
     </div>
   )
 }
