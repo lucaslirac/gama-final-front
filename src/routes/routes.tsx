b1af62586493348387905926b239dbc95673d700
Lucas <lucas_lirac@outlook.com>
2023-03-11 13:23:38 -0300

    new routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,3 +1,4 @@
+import { useContext } from 'react';
 import { Routes as WrapperRoutes, Route, BrowserRouter } from 'react-router-dom'
 
 import Home from '../pages/home/home'
@@ -9,10 +10,20 @@ import Carrinho from '../pages/cart'
 import Admin from '../pages/admin'
 import Check from '../pages/checkout'
 import RegisterProdu from '../pages/RegisterProduct'
-
+import { AuthContext } from '../context/AuthProvider/AuthProvider';
 
 
 export default function Routes() {
+    const { auth } = useContext(AuthContext);
+    /**
+     * Authorized
+     * 
+     * se Auth existe => Redirecionar para Home.
+     * se Auth não existe => Redirecionar para Login.
+     * 
+     * how redirect auth react-router-dom
+     */
+
     return(
         <BrowserRouter>
             <WrapperRoutes>
@@ -22,7 +33,7 @@ export default function Routes() {
             <Route path="/Login" element={<Login />} />
             <Route path="/Products" element={<Products/>} />
             <Route path="/Cart" element={<Carrinho/>} />
-            <Route path="/Admin" element={<Admin/>} />
+            {auth?.isAdmin && <Route path="/Admin" element={<Admin/>} /> }
             <Route path="/Check" element={<Check/>} />
             <Route path="/NewProduct" element={<RegisterProdu/>} />
             </WrapperRoutes>
