1d9d1434df752573e7f0e44ab5bb5449f1065960
Lucas <lucas_lirac@outlook.com>
2023-03-11 13:15:14 -0300

    LocalStore

---
+++ src/service/localStorage.ts
@@ -0,0 +1,27 @@
+import { IAuth } from "../types/index";
+
+
+export function setItemLocalStorage(key: string, item: string) {
+  localStorage.setItem(key, JSON.stringify(item));
+}
+
+export function getItemLocalStorage(key: string) {
+  const item = localStorage.getItem(key)
+
+  return item;
+}
+
+export function cleanLocalStorage() {
+  localStorage.clear();
+}
+
+export function setAuthLocalStorage(user?: IAuth) {
+  if (!user) return;
+  setItemLocalStorage("auth", JSON.stringify(user));
+}
+
+export function getAuthLocalStorage(): IAuth | null {
+  const user = getItemLocalStorage("u")
+  
+  return user ? JSON.parse(user) : null;
+}
