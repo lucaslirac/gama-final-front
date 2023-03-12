e9e987fb9640625b1c0e3e0c1d884bcb69d59c10
Lucas <lucas_lirac@outlook.com>
2023-03-12 15:45:33 -0300

    Admin product list

---
+++ src/pages/PageProductList/index.tsx
@@ -0,0 +1,42 @@
+import { useNavigate } from "react-router-dom";
+import Footer from "../../components/footer";
+import Header from "../../components/header";
+
+import ProductList from "../../components/produtos/ProductList";
+import PProductList from "../../components/ProductList/ListProduct";
+import UserList from "../../components/UserList/UserList";
+
+
+
+
+function PageProductList() {
+    const navigate = useNavigate();
+    return (
+        <div className="App">
+
+            <header>
+                <Header />
+            </header>
+
+            <div className="cta">
+                <h1>Painel de Produtos</h1>
+                <button onClick={() => navigate("/Admin")}>Voltar para admin</button>
+                
+                
+            </div>
+
+            <div>
+                <div>
+                    <PProductList  />
+                </div>
+            </div>
+            <footer>
+                <Footer />
+            </footer>
+
+        </div>
+
+    )
+}
+
+export default PageProductList;
