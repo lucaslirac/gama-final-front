0483c1c7fdff7c5d9df745ae7ea08d425416701f
Lucas <lucas_lirac@outlook.com>
2023-03-12 15:47:28 -0300

    button para voltar pra pagina admin

--- src/pages/RegisterProduct/index.tsx
+++ src/pages/RegisterProduct/index.tsx
@@ -2,13 +2,19 @@ import React from "react";
 import Header from "../../components/header";
 import Footer from "../../components/footer";
 import RegisterProduct from "../../components/NewProductForm";
+import { useNavigate } from "react-router-dom";
 
 
 function RegisterProdu(){
+    const navigate = useNavigate();
     return(
         <div className="App">
         <Header/>
        <div>
+        <div className="cta">
+        <h1>Criar Produto</h1>
+                <button onClick={() => navigate("/Admin")}>Voltar para admin</button>
+        </div>
         <div>
             <RegisterProduct />
         </div>
