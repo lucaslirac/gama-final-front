370688e55c24e33f12af7f3e677d78393abc52f1
Lucas <lucas_lirac@outlook.com>
2023-03-13 15:35:12 -0300

    User edit form

---
+++ src/components/EditUserForm/indext.tsx
@@ -0,0 +1,112 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { redirect, useNavigate } from 'react-router-dom'
+
+import {
+    MDBBtn,
+    MDBContainer,
+    MDBRow,
+    MDBCol,
+    MDBCard,
+    MDBCardBody,
+    MDBInput,
+    MDBIcon,
+    MDBCheckbox
+}
+    from 'mdb-react-ui-kit';
+import { getUserById, signIn } from '../../service/api';
+import { AuthContext } from '../../context/AuthProvider/AuthProvider';
+import { User } from '../../types';
+
+
+interface EditLoginProps {
+  id: string
+}
+
+function EditLogin({id}: EditLoginProps) {
+
+  
+  const [user, setUser] = useState<{} | User>({})
+  console.log(id)
+  
+  useEffect(() => {
+    const editUser = async () => {
+      const userData = await getUserById(id)
+      console.log(userData)
+      setUser(userData.user)
+    }
+    editUser()
+  }, [])
+ 
+
+    return (
+      
+        <MDBContainer
+        fluid
+        className="d-flex align-items-center justify-content-center bg-image mt-5"
+      >
+        <div className="mask gradient-custom-3"></div>
+        <MDBCard className="m-5" style={{ maxWidth: "600px" }}>
+          <MDBCardBody className="px-5">
+            <h2 className="text-uppercase text-center mb-5">Editar Usuario</h2>
+            <MDBInput
+              wrapperClass="mb-4"
+              label="Novo Nome"
+              size="lg"
+              id="name"
+              type="text"
+              value={user.name}
+              //onChange={HandleEmailChange}
+            />
+            <MDBInput
+              wrapperClass="mb-4"
+              label="Novo Email"
+              size="lg"
+              id="email"
+              type="email"
+             
+              //onChange={HandleEmailChange}
+            />
+            <MDBInput
+              wrapperClass="mb-4"
+              label="Nova Senha"
+              size="lg"
+              id="password"
+              type="password"
+              //onChange={handlePasswordChange}
+            />
+             <MDBInput
+              wrapperClass="mb-4"
+              label="Nova funcao"
+              size="lg"
+              id="Role"
+              type="text"
+              //onChange={handlePasswordChange}
+            />
+            <div className="d-flex flex-row justify-content-center mb-4">
+              <MDBCheckbox
+                name="flexCheck"
+                id="flexCheckDefault"
+                label="Concordo com os termos de contrato"
+              />
+            </div>
+           
+            <MDBBtn
+              className="mb-4 w-100 gradient-custom-4"
+              size="lg"
+              onClick={async () => {
+                //await loginUser();
+            
+                
+              
+              }}
+            >
+              
+              Editar
+            </MDBBtn>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBContainer>
+    )
+            }
+
+export default EditLogin;
