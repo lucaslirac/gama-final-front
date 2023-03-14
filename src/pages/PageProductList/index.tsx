6bfbac5740e66a86e0e7d8cc6d7c75d3a40ff5ec
Lucas <lucas_lirac@outlook.com>
2023-03-14 19:16:08 -0300

    funcao  editar funcionando

--- src/pages/PageProductList/index.tsx
+++ src/pages/PageProductList/index.tsx
@@ -5,11 +5,16 @@ import Header from "../../components/header";
 import ProductList from "../../components/produtos/ProductList";
 import PProductList from "../../components/ProductList/ListProduct";
 import UserList from "../../components/UserList/UserList";
+import { deleteProduct } from "../../service/api";
 
 
 
 
 function PageProductList() {
+
+
+
+
     const navigate = useNavigate();
     return (
         <div className="App">
