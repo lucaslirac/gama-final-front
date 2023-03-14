2f7bcbd00e3c5af30c0fe0c6cb8a7dfd88add6e4
Lucas <lucas_lirac@outlook.com>
2023-03-14 19:16:42 -0300

    melhorando card

--- src/components/Productcard/index.tsx
+++ src/components/Productcard/index.tsx
@@ -4,15 +4,21 @@ import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/esm/Row';
 import Col from 'react-bootstrap/esm/Col';
 import * as C from './index.style';
+import { Product } from '../../types';
+import got from "../../assets/got.jpg"
 
 
 import ProductList from '../produtos/ProductList';
+import { useNavigate, useParams } from 'react-router-dom';
+import { AuthContext } from '../../context/AuthProvider/AuthProvider';
+import { useEffect, useState } from 'react';
+import { getProductById } from '../../service/api';
 
 
 
 
 type ProductProps = {
-  id: number;
+  _id: string;
   name: string;
   description: string;
   price: number;
@@ -30,34 +36,15 @@ type ProductProps = {
 
 function ProductCard(props: ProductProps) {
 
-  //onClick={}
-  /**
-   * 
-   * 0) atualizar productCard para ter o props._id
-   * 
-   * 1) localStorage se tem cart e cart._id
-   * 
-   * 2) cartId
-   * 2.1) Se não tem cart, create cart
-   * 
-   * products: [_id: string]
-   * 
-   * handleOnClick = () => {}
-   * 
-   * tem carrinho?
-   * 
-   * não => async createOrder() => [product_id]. (OBS: Criar função de createOrder([product_id])).
-   * 
-   * tem carrinho?
-   * 
-   * sim. Você tem que ter um cart._id (OBS: Criar função de updateOrder(cartId, [product_id])).
-   * onClick={() => handleOnclick(props._id)}
-   */
+  const navigate = useNavigate();
+
+
+
   return (
     <C.Container>
       <Card className="container container-lg, container-sm, container-sm">
         <div className="card ">
-          <img src={props.image} className="card-img-top d-flex" alt="..." />
+          <img src={got} className="card-img-top d-flex" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{props.name}</h5>
           </div>
@@ -65,8 +52,8 @@ function ProductCard(props: ProductProps) {
             <li className="list-group-item">Preço: R$: {props.price}</li>
           </ul>
           <div className="card-body links">
-            <a href="#" className="card-link">Ver Livro</a>
-            <button className="card-link" >Adicionar ao Carrinho</button>
+            <a href="#" className="card-link"onClick={() => navigate(`/ProductPage/${props._id}`)}>Ver Livro</a>
+            <button className="card-link"onClick={() => navigate("/Cart")} >Adicionar ao Carrinho</button>
           </div>
         </div>
       </Card>
